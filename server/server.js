bdd2bed75d248f44fce22f13e2036b686311388b
jsmichot <jsmichot@gmail.com>
2018-02-05 16:47:34 -0600

    changes static route

--- server/server.js
+++ server/server.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const items = require('../database-mongo');
+const items = require('../database/mongo.js');
 const app = express();
 
-app.use(express.static(__dirname + '/../angular-client'));
+app.use(express.static(__dirname + '/../client/'));
 app.use(express.static(__dirname + '/../node_modules'));
 
-app.get('/items', (req, res) => {
-  items.selectAll( (err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+
 
 const port = process.env.PORT || 3000;
 
