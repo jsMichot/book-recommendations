84720d3563200869b37ba1236c811ba1a64337bb
jsmichot <jsmichot@gmail.com>
2018-02-06 03:08:09 -0600

    s

--- angular-client/components/app.js
+++ angular-client/components/app.js
@@ -1,25 +1,12 @@
 angular.module('app')
 .controller('AppCtrl', function(itemsService) {
   const app = this;
-  itemsService.getAll((html) => {
-    app.items = html;
+  itemsService.getAll((xml) => {
+    app.items = xml;
   });
-  app.getQ = (q) => {
-    $http({
-      method: 'GET',
-      url: 'https://www.goodreads.com/search',
-      params: {
-        key: 'Ya50zfsGd2qjCZfprdN5BQ',
-        q: q,
-        part: 'best_book',
-        maxResults: 5,
-        type: 'Book'
-      }
-    })
-      .then(books => {
-        app.items = books;
-      })
-  };
+  itemsService.getQ(q, (xml) => {
+    app.items = xml;
+  })
 })
 .component('app', {
   bindings: {},
--- angular-client/services/items.js
+++ angular-client/services/items.js
@@ -11,4 +11,25 @@ angular.module('app')
       console.log(err);
     });
   };
+  this.getQ = (q, callback) => {
+    $http({
+      method: 'GET',
+      url: 'https://www.goodreads.com/search',
+      params: {
+        key: 'Ya50zfsGd2qjCZfprdN5BQ',
+        q: q,
+        part: 'best_book',
+        maxResults: 5,
+        type: 'Book'
+      }
+    })
+    .then(function (xml) {
+      if (callback) {
+        callback(xml);
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  };
 });
