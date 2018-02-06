d169d2cb1a5df4f4e9185c63323b1eb45cb63443
jsmichot <jsmichot@gmail.com>
2018-02-06 11:52:30 -0600

    s

--- angular-client/services/items.js
+++ angular-client/services/items.js
@@ -23,20 +23,14 @@ angular.module('app')
         "Postman-Token": "e0134111-8c4d-3124-0eb6-0e192384ae76"
       },
       "processData": false,
-      "data": "{\n\t\"q\": \"romance\"\n}"
+      "data": "{\n\t\"q\": q\n}"
     }
     $.ajax(settings).done(function (response) {
       console.log(response);
-    })
-    .then((books) => {
-      console.log(books);
       if (callback) {
         callback(books);
       }
     })
-    .catch(function (err) {
-      console.log(err);
-    });
     // $http({
     //   url: '/books',
     //   method: "POST",
