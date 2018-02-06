092892ce22802f2456ed1a1d9589fcf7d11edf68
jsmichot <jsmichot@gmail.com>
2018-02-05 22:27:11 -0600

    test 14

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const $http = require('http');
+const request = require('request');
 
 const items = require('../database-mongo');
 
