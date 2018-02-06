026e4649f489f7392dadfdb6caa4cc5e9ed8a7b8
jsmichot <jsmichot@gmail.com>
2018-02-05 22:49:26 -0600

    inspecting api data

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get('/books', (req, res) => {
       console.log('SERVER error: ' + error);
     } else {
       parseString(body, function (err, result) {
-        console.log(result);
+        console.log(JSON.stringify(result));
       });
     }
   })
